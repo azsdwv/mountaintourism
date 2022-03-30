052182086792a99b73a5a9fe6f3ad8f9304fcc7a
asddsa <94194669+azsdwv@users.noreply.github.com>
2022-03-30 19:57:31 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
   $('.burger').click(function (event) {
       $('.burger, .activ, nav').toggleClass('active')
-  })
+  });
+  $(window).scroll(function () {
+        if ($(window).scrollTop() > 300) {
+            $('#top').fadeIn();
+        } else {
+            $('#top').fadeOut();
+        }
+    });
+    $('#top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
 })
